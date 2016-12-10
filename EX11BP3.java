84fa8671e2bbd162cd45ff342ba703748dee8db7
lailaw <lailawert@rocketmail.com>
2016-12-09 22:02:53 -0500

    Create  EX11BP3.java

---
+++ EX11BP3.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+//import java.io.*;
+public class EX11BP3 {
+
+	public static void main(String[] args)throws Exception
+	{
+		
+		 InputStreamReader r=new InputStreamReader(System.in);  
+		 BufferedReader br=new BufferedReader(r); 
+		// TODO Auto-generated method stub
+
+int firstinteger, secondinteger, gcf, lowernumber;
+
+System.out.println("Enter an integer: ");  
+name=br.readLine(); 
+System.out.println("number is "+firstinteger);
+//value1 = Integer.valueof(name);
+firstinteger = Integer.parseInt(name);
+
+System.out.println("Enter an integer"); 
+name =br.readLine();
+System.out.println("number is "+secondinteger);
+//value2 = Integer.valueof(name);
+secondinteger = Integer.parseInt(name);
+
+if(firstinteger>=second integer){
+secondinteger=lowernumber;}
+if(firstinteger<=secondinteger){
+firstinteger=lowernumber;}
+for(count=lowernumber;count--){
+=firstinteger/count
