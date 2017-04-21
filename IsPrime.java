fc4087d5dd906ca1a254c221b2aeed3a951c56cb
lailaw <lailawert@rocketmail.com>
2017-04-20 21:54:07 -0400

    Create IsPrime.java

---
+++ IsPrime.java
@@ -0,0 +1,7 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+//import java.io.*;
+public class IsPrime.java{
+
+	public static void main(String[] args)throws Exception
+long=0;
