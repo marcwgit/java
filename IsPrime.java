bc73d948ff19118a9175f68602704c3b2e6f5555
lailaw <lailawert@rocketmail.com>
2017-04-20 21:55:57 -0400

    Update IsPrime.java

--- IsPrime.java
+++ IsPrime.java
@@ -5,3 +5,8 @@ public class IsPrime.java{
 
 	public static void main(String[] args)throws Exception
 long=0;
+System.out.println("Enter an integer: ");  
+name=br.readLine(); 
+System.out.println("number is "+name);
+//value1 = Integer.valueof(name);
+firstinteger = Integer.parseInt(name);
