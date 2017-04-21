2ff9b078e744bd7ec75e54d8925040583a80dcac
lailaw <lailawert@rocketmail.com>
2017-04-20 22:41:19 -0400

    Update IsPrime.java

--- IsPrime.java
+++ IsPrime.java
@@ -5,6 +5,7 @@ public class IsPrime.java{
 
 	public static void main(String[] args)throws Exception
 long userinput=0, maxinput=0;
+	boolean result=true; //true means number is prime.
 System.out.println("Enter an integer: ");  
 name=br.readLine(); 
 System.out.println("number is "+name);
@@ -14,5 +15,7 @@ userinput= Long.parseLong(name);
 	int j=maxinput.intValue();
 	System.out.println("Value of j "+J+");
 		for(int count=2; j;count++){
-			
+			if(userinput%2=0){
+			result=false;
+			}
 		}
