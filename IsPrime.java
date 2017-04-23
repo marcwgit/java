e4d1186b0f1d4be545000e22c34b071b666e6a19
lailaw <lailawert@rocketmail.com>
2017-04-23 12:05:26 -0400

    Update IsPrime.java

--- IsPrime.java
+++ IsPrime.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.lang;
+import java.lang.*;
 public class IsPrime{
 
 	public static void main(String[] args)throws Exception{
